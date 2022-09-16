d1079355fac236ec48102cdf75adc38e2d061b05
DanielN77 <113138097+DanielN77@users.noreply.github.com>
2022-09-16 10:19:44 +0200

    Delete incorrectly named directory

--- sum_num/target/debug/deps/sum_num-d4a8b693345dd1f3.d
+++
@@ -1,5 +0,0 @@
-/home/dnorma/Rust/dnorma-task-2/sum_num/target/debug/deps/sum_num-d4a8b693345dd1f3: src/main.rs
-
-/home/dnorma/Rust/dnorma-task-2/sum_num/target/debug/deps/sum_num-d4a8b693345dd1f3.d: src/main.rs
-
-src/main.rs:
--- sum_num/target/debug/sum_num.d
+++
@@ -1,1 +0,0 @@
-/home/dnorma/Rust/dnorma-task-2/sum_num/target/debug/sum_num: /home/dnorma/Rust/dnorma-task-2/sum_num/src/main.rs
